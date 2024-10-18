aff9414bba3fad9c530b2c2ba0c64655937519e5
GiovannaGrigo <giovanna1grigo@gmail.com>
2024-10-17 22:15:48 -0300

    Criando os primeiros botões

--- components/Botao.js
+++ components/Botao.js
@@ -1,13 +1,13 @@
-import { TouchableHighlight, 
-    StyleSheet, 
-    Text } from "react-native"
+import { TouchableHighlight, StyleSheet, Text, Dimensions } from "react-native"
 
-const Botao = () => {
+const {width, height} = Dimensions.get('window')
+
+const Botao = (props) => {
     return (
         <TouchableHighlight
-            onPress={() => console.log(`clicou no botão`)}>
+            onPress={() => console.log(`clicou no botão ${props.label}`)}>
             <Text style={estilos.Botao}>
-                3
+                {props.label}
             </Text>
         </TouchableHighlight>
     )
@@ -15,9 +15,9 @@ const Botao = () => {
 
 const estilos = StyleSheet.create({
     Botao: {
-        fontSize: 40,
-        width: 50,
-        height: 50,
+        fontSize: 50,
+        width: width / 4,
+        height: width / 4,
         backgroundColor: '#f0f0f0',
         textAlign: 'center',
         borderWidth: 1,
